870827de022d0fc1fe97549990a385785fb06572
Mizuki Sango <msageha@yahoo.co.jp>
2018-02-23 03:26:30 +0900

    add

--- server/line_bot_server.py
+++ server/line_bot_server.py
@@ -31,20 +31,11 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-from monitor_var_log import (
-    reject_ssh, allow_ssh
-)
-# import getting_pc_info
 import token_key
-# import notification
-# import message_dicts
+from predict import Predict
 
 app = Flask(__name__)
-
-# get channel_secret and channel_access_token from your environment variable
-# channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_secret = token_key.channel_secret
-# channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 channel_access_token = token_key.channel_access_token
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
@@ -55,6 +46,53 @@ if channel_access_token is None:
 
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
+predict = Predict(model_file='2018-02-22 20:48:27_epoch-16.npz', gpu=0)
+
+@app.route("/")
+def hello_world():
+  return "Hello World!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        print('-------------')
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+        message = str(event)
+        if event.message.text:
+            text = event.message.text.strip()
+            replay = predict(text)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=replay)
+            )
+
+    return 'OK'
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', default=5000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    args = arg_parser.parse_args()
+
+    app.run(debug=args.debug, port=args.port)
 
 # def get_ip(word):
 #     m = re.search(r'([0-9\.]+)', word)
@@ -97,51 +135,3 @@ parser = WebhookParser(channel_secret)
 #     print(f'message:::{message}, type:::{type(message)}')
 #     return message
 
-@app.route("/")
-def hello_world():
-  return "Hello World!"
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    # for event in events:
-    #     print('-------------')
-    #     if not isinstance(event, MessageEvent):
-    #         continue
-    #     if not isinstance(event.message, TextMessage):
-    #         continue
-    #     message = str(event)
-    #     if event.message.text:
-    #         text = event.message.text.strip()
-    #         message = text_processing(str(text))
-    #         print(f'message:::{message}')
-    #         if message == '':
-    #             continue
-    #         line_bot_api.reply_message(
-    #             event.reply_token,
-    #             TextSendMessage(text=message)
-    #         )
-
-    return 'OK'
-
-if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=5000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
--- server/test.py
+++
@@ -1,5 +0,0 @@
-
-import time
-
-print('start')
-print('exit')
