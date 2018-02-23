5e81e8a521b5e116b6b2056e0d8a2a638eb21886
Mizuki Sango <msageha@yahoo.co.jp>
2018-02-23 21:24:13 +0900

    add

--- client/reading_file.py
+++ client/reading_file.py
@@ -8,24 +8,23 @@ def read_file_continue():
         # print(line, end='')
         #Oct  8 09:08:25 raspberrypi sshd[10358]: Failed password for root from 186.42.238.101 port 38352 ssh2
         if 'sshd' in line:
+            print(line)
             if 'Failed' in line:
                 failed_continue_times += 1
                 if failed_continue_times == 2:
-                    if len(line.split()) < 14:
-                        user_name = line.split()[8]
-                        from_ip = line.split()[10]
-                        text = f'{from_ip} が{user_name}の認証失敗したよ'
-                        print(text)
-                        request_message.send_message(text)
+                    user_name = line.split()[8]
+                    from_ip = line.split()[10]
+                    text = f'{from_ip} が{user_name}の認証失敗したよ'
+                    print(text)
+                    request_message.send_message(text)
             else:
                 failed_continue_times = 0
             if 'Accepted' in line:
-                if len(line.split()) < 14:
-                    user_name = line.split()[8]
-                    from_ip = line.split()[10]
-                    text = f'{user_name}が{from_ip}からログインしたよ'
-                    print(text)
-                    request_message.send_message(text)
+                user_name = line.split()[8]
+                from_ip = line.split()[10]
+                text = f'{user_name}が{from_ip}からログインしたよ'
+                print(text)
+                request_message.send_message(text)
 
 if __name__ =='__main__':
     read_file_continue()
